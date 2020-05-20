b58545e367fe8d865c3138ed843e0354231b9350
Ben Cole <benedictcole1@gmail.com>
2020-05-20 10:47:59 +0930

    added .prompt switch statement to direct choices

--- prompt.js
+++ prompt.js
@@ -22,31 +22,53 @@ const inquirer = require("inquirer");
 
 // Exit program: 
 
-// 
-async function viewAllEmployees() {
-    const userChoice = await mainMenu();
-}
 
-viewAllEmployees()
-
-
-function mainMenu() { 
-
-return inquirer
-    .prompt([
-        {
-            type: 'list',
-            name: 'choice',
-            message: 'What would you like to do?',
-            choices: ['View all employees', 'View employees by department', 'View employees by manager', 'Add employee', 'Remove employee', 'Update employee role', 'Update employee manager', 'Exit'],
-        }
-    ]);
-
+function mainMenu() {
+    return inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'choice',
+                message: 'What would you like to do?',
+                choices: ['View all employees', 'View employees by department', 'View employees by manager', 'Add employee', 'Remove employee', 'Update employee role', 'Update employee manager', 'Exit'],
+            }
+        ]);
 }
 
 
+async function mainMenuChoice() {
+    const mainResponse = await mainMenu(); // Get user's main menu choice and store this object in mainResponse.
+    const mainChoice = mainResponse['choice']; // Let mainChoice equal user choice value 'string'
 
+    switch (mainChoice) {
+        case 'View all employees':
+            // function
+            break;
+        case 'View employees by department':
+            // function
+            break;
+        case 'View employees by manager':
+            // function
+            break;
+        case 'Add employee':
+            // function
+            break;
+        case 'Remove employee':
+            // function
+            break;
+        case 'Update employee role':
+            // function
+            break;
+        case 'Update employee manager':
+            // function
+            break;
+        case 'Exit':
+            // function
+            break;
+    }
+}
 
+mainMenuChoice()
 
 
 
