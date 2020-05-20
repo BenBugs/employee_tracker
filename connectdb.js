b9cb9ffd3179e46dc3f91d6f92bdfe87f6b00688
Ben Cole <benedictcole1@gmail.com>
2020-05-20 16:59:32 +0930

    working server call

---
+++ connectdb.js
@@ -0,0 +1,58 @@
+// Class to use when making database calls.
+const cTable = require('console.table');
+const mysql = require("mysql");
+
+class ConnectDB {
+    constructor() {
+
+        // Connection object.
+        this.connection = mysql.createConnection(
+            {
+                host: 'localhost',
+                user: 'root',
+                port: 3306,
+                password: 'M1l3sd4v1s!',
+                database: 'employee_tracker'
+            }
+        )
+    }
+
+    // Wrap connection.connect() in a promise.
+    async connect() {
+        return new Promise((resolve, reject) => {
+            this.connection.connect(err => {
+                if (err) reject(err); // oh no!
+                else resolve(); // oh yeah!
+            })
+        })
+    }
+
+    // Wrap connection.query() in a promise.
+    async query(command, values) {
+        return new Promise((resolve, reject) => {
+            this.connection.query(command, values, (error, results) => {
+                if (error) reject(error); // nay!
+                else resolve(results); // yay!
+            })
+        })
+    }
+
+    // End connection.
+    end() {
+        this.connection.end();
+    }
+}
+
+// async function employeeTableTest() {
+//     const test = new ConnectDB()
+//     let newConnect = test.connect();
+//     let newQuery = await test.query(`SELECT * FROM employee INNER JOIN role INNER JOIN department`);
+//     console.table(newQuery);
+//     newConnect = test.end();
+// }
+
+// employeeTableTest()
+
+
+
+module.exports = ConnectDB;
